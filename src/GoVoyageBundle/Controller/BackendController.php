9ac3e319ec7770fde19768980eb1556293739c94
Asus <wajdy.bouslama@esprit.tn>
2017-11-21 00:59:11 +0100

    Voyage personalisé Admin

--- src/GoVoyageBundle/Controller/BackendController.php
+++ src/GoVoyageBundle/Controller/BackendController.php
@@ -10,6 +10,8 @@ namespace GoVoyageBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GoVoyageBundle\Entity\Voyagepersonalise;
+use Symfony\Component\HttpFoundation\Request;
 
 class BackendController extends Controller
 {
@@ -18,4 +20,21 @@ class BackendController extends Controller
         return $this->render('GoVoyageBundle:Admin:index_admin.html.twig');
     }
 
+    public function vpAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em2 = $this->getDoctrine()->getManager();
+        $voyagepersonalises = $em->getRepository('GoVoyageBundle:Voyagepersonalise')->findAll();
+        return $this->render('GoVoyageBundle:Admin:voyagepersonaliseadmin.html.twig', array(
+            'voyagepersonalises' => $voyagepersonalises
+        ));
+    }
+    public function SupprAction($id)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $vol=$em->getRepository("GoVoyageBundle:Voyagepersonalise")->find($id);
+        $em->remove($vol);
+        $em->flush();
+        return $this->redirectToRoute('Backend_vp');
+    }
 }
